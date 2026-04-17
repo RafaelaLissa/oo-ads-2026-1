c6c171d8a07dd2d226f0a4b612743ab8c74a9714
rafae <paraaplicativos259@gmail.com>
2026-04-17 09:14:28 -0300

    "(17/04) Privatização de atributos"

--- empresa/src/ContaBancaria.java
+++ empresa/src/ContaBancaria.java
@@ -40,17 +40,51 @@ public class ContaBancaria {
 
     // Atributos
     // Protected: apenas classes do mesmo pacote têm acesso
-    protected Double saldo;
-    protected Double saldo = 0.0;
+    private Integer numConta;
     public String titular;
+    protected Double saldo = 0.0;
+    private String titular;
+    private Double saldo = 0.0;
 
     // Construtor personalizado
-    public ContaBancaria(String titular, Double saldoInicial) {
+    public ContaBancaria(Integer numConta, String titular, Double saldoInicial) {
+        //this.numConta = numConta;
+        this.setNumConta(numConta);
         this.titular = titular;
+        //this.titular = titular;
+        this.setTitular(titular);
         //this.saldo = saldoInicial;
         // Chama depositar() para ajustar o saldo inicial da conta,
         // passando pela validação existente no método
         this.depositar(saldoInicial);
+    }
+
+    // Getters/setters
+    public Integer getNumConta() {
+        return this.numConta;
+    }
+
+    public void setNumConta(Integer novoNumConta) {
+        if(novoNumConta <= 0) {
+            throw new IllegalArgumentException("O número da conta deve ser maior que zero.");
+        }
+        this.numConta = novoNumConta;
+    }
+
+    public String getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(String novoTitular) {
+        // Este é um exemplo de setter sem validações
+        this.titular = novoTitular;
+    }
+
+    // O atributo saldo terá apenas o getter para podermos
+    // consultar o seu valor. As modificações do seu valor
+    // deverão ser feitas pelos métodos depositar() e sacar()
+    public Double getSaldo() {
+        return this.saldo;
     }
 
     // Métodos
@@ -71,6 +105,7 @@ public class ContaBancaria {
     }
 
     public void exibirSaldo() {
-        System.out.println("Saldo da conta de " + this.titular + " é R$ " + this.saldo);
+        String msg = "Saldo da conta bancária nº %d de %s é R$ %.2f".formatted(this.numConta, this.titular, this.saldo);
+        System.out.println(msg);
     }
 }
--- empresa/src/ContaEspecial.java
+++ empresa/src/ContaEspecial.java
@@ -26,19 +26,50 @@ public class ContaEspecial extends ContaBancaria {
 
     // Atributo
     Double limite;
+    private Double limite;
 
     // Construtor personalizado
-    public ContaEspecial(String titular, Double saldoInicial, Double limite) {
-        super(titular, saldoInicial);
+    public ContaEspecial(
+            Integer numConta,
+            String titular,
+            Double saldoInicial,
+            Double limite
+    ) {
+        super(numConta, titular, saldoInicial);
         this.limite = limite;
+        //this.limite = limite;
+        this.setLimite(limite);
+    }
+
+    // Getter e setter
+    public Double getLimite() {
+        return this.limite;
+    }
+
+    public void setLimite(Double novoLimite) {
+        if(novoLimite <= 0) {
+            throw new IllegalArgumentException("O limite deve ser maior que zero.");
+        }
+        this.limite = novoLimite;
     }
 
     @Override
     public void exibirSaldo() {
-        System.out.println("Saldo da conta especial de " + this.titular + " é R$ " + (this.saldo + this.limite));
+        String msg = "Saldo da conta ESPECIAL nº %d de %s é R$ %.2f".formatted(this.getNumConta(), this.titular, this.saldo + this.limite);
+        String msg = "Saldo da conta ESPECIAL nº %d de %s é R$ %.2f".formatted(
+                this.getNumConta(),
+                this.getTitular(),
+                this.getSaldo() + this.getLimite()
+        );
+        System.out.println(msg);
     }
-
     public void exibirSaldoReal() {
-        System.out.println("Saldo (real) da conta especial de " + this.titular + " é R$ " + this.saldo);
+        String msg = "Saldo (real) da conta ESPECIAL nº %d de %s é R$ %.2f".formatted(this.getNumConta(), this.titular, this.saldo);
+        String msg = "Saldo (real) da conta ESPECIAL nº %d de %s é R$ %.2f".formatted(
+                this.getNumConta(),
+                this.getTitular(),
+                this.getSaldo()
+        );
+        System.out.println(msg);
     }
 }
